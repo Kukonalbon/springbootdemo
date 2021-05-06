d7bbf9499d92302d23ce7491e6670ad705b1c0a9
Kukonalbon <yz444700699114787@163.com>
2021-05-06 20:57:46 +0800

    a

--- src/main/java/com/xh/controll/AdminControl.java
+++ src/main/java/com/xh/controll/AdminControl.java
@@ -1,38 +1,20 @@
 package com.xh.controll;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.xh.entiy.Course;
-import com.xh.service.CourseService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminControl {
-	
-	@Autowired
-	private CourseService courseService;
-	
+
 	@RequestMapping(value="/addcourse",method=RequestMethod.POST)
 	@ResponseBody
 	public String addCourse(Course course){
 		System.out.println(course.getName()+"  "+ course.getDescript());
 		return "lll";
-	}
-	
-	@RequestMapping(value="/getcourse",method=RequestMethod.GET)
-	@ResponseBody
-	public List<Course> getCourse(){
-		System.out.println("**");
-		List<Course> courses=courseService.getCourses();
-		for (Course c : courses) {
-			System.out.println(c.getId()+"  "+c.getName()+"  "+c.getDescript()+"  "+c.getCreate_time());
-		}
-		return courses;
 	}
 }
--- src/main/java/com/xh/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xh.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.xh.entiy.Course;
-
-public interface CourseRepository extends CrudRepository<Course, Integer>{
-
-}
